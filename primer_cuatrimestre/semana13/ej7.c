582cd2f86c9da66fdc6115cd431c471a512dec35
Francisco Risso <Franciscorisso05@gmail.com>
2025-07-30 15:14:53 -0300

    ultimos arreglos

--- primer_cuatrimestre/semana13/ej7.c
+++ primer_cuatrimestre/semana13/ej7.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #define max 10
-void cargaDatos (float precio[max], char nombre[max][30], int cantidad) {
+void cargaDatos (float precio[max], char nombre[max][30], int *cantidad) {
         int i = 0;
         do {
         printf ("ingrese el nombre del producto/n");
@@ -18,25 +18,79 @@ void cargaDatos (float precio[max], char nombre[max][30], int cantidad) {
         char seguir;
         printf ("usted desea seguir ingresando? (si/no):");
         scanf ("%c", &seguir);
-        if (seguir != "si" && seguir != "SI"){
+        if (strcmp(seguir, "si")  != 0 && strcmp(seguir, "SI") != 0){
             break;
         }
 
         } while ( i < max);
-        cantidad = i; 
+        *cantidad = i; 
 }
 
-void calculos ( float precio[max], char nombre[max][30], float mayor, float total, float promedio) {
-    mayor = precio[0];
-    total = 0;
+void calculos ( float precio[max], char nombre[max][30], float *mayor, float *total, float *promedio) {
+    *mayor = precio[0];
+    *total = 0;
     for (int i = 0; i < max; i++) {
-        total += precio[i];
-        if (precio[i] > mayor) {
-            mayor = precio[i];
+        *total += precio[i];
+        if (precio[i] > *mayor) {
+            *mayor = precio[i];
         }
     }
-    promedio = total/max;
+    *promedio = *total/max;
+}
+
+int buscar(float nombres[max][30], int cantidad, char buscado[]) {
+    for (int i = 0; i < max; i++) {
+        if(strcmp(nombres[i], buscado) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void mostrar(char nombres[][30], float precios[], int cantidad) {
+    if (cantidad == 0) {
+        printf("No hay productos cargados.\n");
+        return;
+    }
+
+    int posMayor = 0;
+    printf("\n--- Lista de productos ---\n");
+    for (int i = 0; i < cantidad; i++) {
+        printf("%d. %s - $%.2f\n", i + 1, nombres[i], precios[i]);
+
+        if (precios[i] > precios[posMayor]) {
+            posMayor = i;
+        }
+    }
+
+    printf("\n El producto más caro es: %s - $%.2f\n", nombres[posMayor], precios[posMayor]);
 }
 
 
+int main () {
+    char nombres;
+    int cantidad, opcion;
+    float precios;
+    do {
+        printf ("\n------MENU------\n");
+        printf ("Elija una opcion\n");
+        printf ("1: mostrar todos los productos\n");
+        printf ("2: mostrar producto mas caro\n");
+        printf ("3: mostrar promedio de productos\n");
+        printf ("4: buscar proctos\n");
+        printf ("5: salir\n");
+        scanf ("%d, &opcion");
+
+        switch (opcion) {
+            case 1: mostrar(nombres, cantidad, precios);
+            break;
+
+            case 2: mostrar(nombres, cantidad, precios);
+            break;
+
+            case 3: calculos(nombres, mayor);
+            break;
+        }
+    } while (opcion != 5;)
+}
   
