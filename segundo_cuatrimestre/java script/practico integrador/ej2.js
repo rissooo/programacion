8254d12d29e97cfb5e0ebd2a8b96a203dbffb56e
francisco <franciscorisso05@gmail.com>
2025-09-17 19:12:54 -0300

    ..

---
+++ segundo_cuatrimestre/java script/practico integrador/ej2.js
@@ -0,0 +1,6 @@
+<input type="number" id="celsius" placeholder="Grados Celsius">
+<button id="convertir">Convertir a Fahrenheit</button>
+<p>Resultado: <span id="resultado">--</span> °F</p>
+<script>
+
+</script>
